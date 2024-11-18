f01749f77de860ef49b9a576ed510ea7cd3fc6da
Abhi-0088 <abhilashojha351@gmail.com>
2024-11-18 17:00:55 +0530

    updating CI_Demo.py file

--- CI_Demo.py
+++ CI_Demo.py
@@ -2,16 +2,15 @@ def add(a,b):
     return a+b
 
 
+def sub(a,b):
+    return a-b
 
-
-
-
-def sub(a,b):return a-b
 
 def main():
     a = 10
     b = 10
     print(add(a,b),sub(a,b))
+
 
 if __name__== "__main__":
     main()
